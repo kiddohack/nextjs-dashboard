2425faf6fba8631f3273ea5b22297d388954a687
Gutu Corneliu <sprintyx@gmail.com>
2024-02-18 15:34:37 +0200

    fetched latest invoiced

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,16 +1,14 @@
 import { lusitana } from '@/app/ui/fonts';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
 import {
   fetchCardData,
-  fetchLatestInvoices,
-  fetchRevenue
 } from '../../lib/data';
 import { Card } from '../../ui/dashboard/cards';
 import LatestInvoices from '../../ui/dashboard/latest-invoices';
 import RevenueChart from '../../ui/dashboard/revenue-chart';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices, 
     totalPendingInvoices, 
@@ -33,8 +31,12 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
